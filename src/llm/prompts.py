635b9748086eda8197b74b69bc01322fe59ab4b5
maryamyazdi <yazdimaryam5@gmail.com>
2024-08-21 13:24:39 +0330

    add streamlit demo

--- src/llm/prompts.py
+++ src/llm/prompts.py
@@ -1,6 +1,8 @@
 RAG_prompt_template = """
 You are a helpful AI assistant. Answer the QUESTION based on the CONTEXT retrieved from database.
 You can use your own knowledge too, but make sure to use the facts from the CONTEXT when answering the QUESTION.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+Keep the answer as concise as possible.
 
 QUESTION: {question}
 
